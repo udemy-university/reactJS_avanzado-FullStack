3316141c7d03f0f9c267be54163475bf1dc2d64a
SandroSD <sdezerio@gmail.com>
2019-06-03 00:35:39 -0300

    iniciamos repositorio del curso

---
+++ schema.js
@@ -0,0 +1,26 @@
+import {buildSchema} from 'graphql';
+
+const schema = buildSchema(`
+    type Cliente{
+        id: ID
+        nombre: String
+        apellido: String
+        empresa: String
+        email: String
+    }
+    type Query{
+        cliente: Cliente
+    }
+    input ClienteInput {
+        id: ID
+        nombre: String!
+        apellido: String!
+        empresa: String!
+        email: String!
+    }
+    type Mutation {
+        crearCliente(input: ClienteInput) : Cliente
+    }
+`);
+
+export default schema;
