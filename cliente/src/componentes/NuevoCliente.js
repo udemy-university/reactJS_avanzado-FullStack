65625ad5641f2b28d3c5f0dc6303f8abf75cf02f
SandroSD <sdezerio@gmail.com>
2019-07-06 22:01:10 -0300

    Ver desde el video 67

--- cliente/src/componentes/NuevoCliente.js
+++ cliente/src/componentes/NuevoCliente.js
@@ -12,20 +12,52 @@ class NuevoCliente extends Component {
 			edad: '',
 			email: '',
 			tipo: ''
-		}
-	 }
-    render() { 
+		},
+		error: false,
+		emails: []
+	}
+
+	nuevoCampo = () => {
+		this.setState({
+			emails: this.state.emails.concat([{email: ''}])
+		})
+	}
+
+	quitarCampo = i => () => {
+		this.setState({
+			emails: this.state.emails.filter((email, index) => i !== index)
+		})
+	}
+
+    render() {
+		const {error} = this.state;
+		let respuesta = error ? <p className="alert alert-danger p-3 text-center">Todos los campos son obligatorios</p> : '';
         return (
             <Fragment>
                 <h2 className="text-center">Nuevo Cliente</h2>
+				{respuesta}
                 <div className="row justify-content-center">
-					<Mutation mutation={NUEVO_CLIENTE}>
+					<Mutation 	mutation={NUEVO_CLIENTE}
+								onCompleted={()=>this.props.history.push('/')}
+					>
 						{crearCliente => (
 							<form className="col-md-8 m-3"
 									onSubmit={e=>{
 										e.preventDefault();
 
 										const {nombre, apellido, empresa, edad, tipo, email} = this.state.cliente;
+
+										if(nombre === '' || apellido === '' || empresa === '' || edad === '' || tipo === ''){
+											this.setState({
+												error: true
+											});
+											return ;
+										}
+
+										this.setState({
+											error: false
+										});
+
 										const input = {nombre, apellido, empresa, edad: Number(edad), tipo, email};
 
 										crearCliente({
@@ -65,7 +97,7 @@ class NuevoCliente extends Component {
 									</div>
 								</div>
 								<div className="form-row">
-									<div className="form-group col-md-6">
+									<div className="form-group col-md-12">
 										<label>Empresa</label>
 										<input	onChange={e=>{
 													this.setState({
@@ -80,20 +112,31 @@ class NuevoCliente extends Component {
 												placeholder="Empresa"
 										/>
 									</div>
-									<div className="form-group col-md-6">
-										<label>Email</label>
-										<input	onChange={e=>{
-													this.setState({
-														cliente: {
-															...this.state.cliente,
-															email: e.target.value
-														}
-													})
-												}}
-												type="email"
-												className="form-control"
-												placeholder="Email"
-										/>
+									{this.state.emails.map((input, index)=> (
+										<div key={index} className="form-group col-md-12">
+											<label>Correo: {index + 1}:</label>
+											<div className="input-group">
+												<input	type="email"
+														placeholder="Email"
+														className="form-control"
+												/>
+												<div className="input-group-append">
+													<button	onClick={this.quitarCampo(index)}
+															type="button"
+															className="btn btn-danger"
+													> &times; Eliminar
+													</button>
+												</div>
+											</div>
+										</div>
+									))}
+									<div className="form-group d-flex justify-content-center col-md-12">
+										<button	onClick={this.nuevoCampo}
+												type="button"
+												className="btn btn-warning"
+										>
+											+ Agregar Email
+										</button>
 									</div>
 								</div>
 								<div className="form-row">
@@ -129,7 +172,7 @@ class NuevoCliente extends Component {
 										</select>
 									</div>
 								</div>
-								<button type="submit" className="btn btn-success float-right">Guardar Cambios</button>
+								<button type="submit" className="btn btn-success float-right">Agregar Cliente</button>
 							</form>
 						)}
 					</Mutation>
